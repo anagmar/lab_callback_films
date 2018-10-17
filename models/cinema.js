3bd35c5a97924b417628473d933163234939bec4
anagmar <gabu2ma@gmail.com>
2018-10-17 15:02:07 +0100

    find film by title

--- models/cinema.js
+++ models/cinema.js
@@ -5,6 +5,8 @@ Cinema.prototype.filmTitles = function() {
   return this.films.map(n => n.title);
 };
 
-
+Cinema.prototype.findFilmByTitle = function(i) {
+  return this.films.find(n => n.title === i);
+}
 
 module.exports = Cinema;
